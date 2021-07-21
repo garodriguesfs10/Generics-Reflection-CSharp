5c3b5acf0a182588b6fe1293d6b537c2607c4ebb
Gabriel Rodrigues <garodriguesfs10@gmail.com>
2021-07-21 10:39:14 -0300

    conceitos Generics e Reflection

---
+++ Generics/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Problema a ser resolvido: Não criar um método cruzar para cada espécie de Mamifero, e sim uma genérica.
+
+            // Código sem Generics
+            //var macho = new Cachorro() { Nome = "João"};
+            //var femea = new Cachorro() { Nome = "Maria" };
+            //var filhote = new Cachorro().CruzarCachorro(macho, femea);
+            //    public Cachorro CruzarCachorro(Cachorro macho, Cachorro femea)
+            //    {
+            //        var filhote = new Cachorro() { Pai = macho, Mae = femea };
+            //        return filhote;
+            //    }
+            //Console.WriteLine($"Filho de {filhote.Pai.Nome} e {filhote.Mae.Nome}");
+
+            // Código com Generics:
+            var machoCachorro = new Cachorro();
+            var femeaCachorro = new Cachorro();
+            var filhote = Cruzar(machoCachorro, femeaCachorro, "cachorro");
+            Console.WriteLine($"Filho do tipo: {filhote.especie}");
+            var machoGato = new Gato();
+            var femeaGato = new Gato();
+            var filhote2 = Cruzar(machoGato, femeaGato, "gato");
+            Console.WriteLine($"Filho do tipo: {filhote2.especie}");
+            var machoElefante = new Elefante();
+            var femeaElefante = new Elefante();
+            var filhote3 = Cruzar(machoElefante, femeaElefante, "elefante");
+            Console.WriteLine($"Filho do tipo: {filhote3.especie}");
+        }
+
+        public static T Cruzar<T>(T macho, T femea, string especie) where T : Mamifero<T>
+        {
+            var filhoteGenerico = Activator.CreateInstance<T>(); // cria um tipo Genérico
+            filhoteGenerico.Mae = femea;
+            filhoteGenerico.Pai = macho;
+            filhoteGenerico.especie = especie;
+            return filhoteGenerico;
+        }
+    }
+
+
+    // Código com Generics
+    public abstract class Mamifero<T> where T : Mamifero<T>
+    {
+
+        public string Nome { get; set; }
+        public T Pai { get; set; }
+        public T Mae { get; set; }
+
+        public string especie { get; set; }
+
+
+    }
+
+    public class Cachorro : Mamifero<Cachorro>
+    {
+        public string Raca { get; set; }
+    }
+
+    public class Gato : Mamifero<Gato>
+    {
+        public int qtdPelos { get; set; }
+    }
+
+    public class Elefante : Mamifero<Elefante>
+    {
+        public string corElefante { get; set; }
+    }
+    // Código Sem Generics
+    //public abstract class Mamifero {       
+
+    //    public string Nome { get; set; }
+    //    public Mamifero Pai { get; set; }
+    //    public Mamifero Mae { get; set; } 
+    //    public decimal Peso { get; set; }
+    //}
+
+    //public class Cachorro : Mamifero
+    //{   
+    //    public string Raca { get; set; }
+
+    //    public Cachorro CruzarCachorro(Cachorro macho, Cachorro femea)
+    //    {
+    //        var filhote = new Cachorro() { Pai = macho, Mae = femea };
+    //        return filhote;
+    //    }
+    //}
+
+    //public class Gato : Mamifero
+    //{
+    //    public int qtdPelos { get; set; }
+    //}
+
+    //public class Elefante : Mamifero
+    //{
+    //    public string corElefante { get; set; }
+    //}
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace Reflection
+{
+    class Program
+    {
+        // Problema a ser resolvido: Criar somente um método de Log pegando todos os atributos da classe referenciada.
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa() { Nome = "Gabriel", Idade = 24, Profissao = "Desenvolvedor" };
+            var equipamento = new Equipamento() { Tipo = "Industrial", Marca = "3M", Quantidade = 8 };
+            var livro = new Livro() { Nome = "Clean Architecture", Autor = "Billie", Edicao = 3 };
+
+            Log(pessoa);
+            Log(equipamento);
+            Log(livro);
+        }
+
+        public static void Log(object obj)
+        {
+
+            var tipo = obj.GetType();
+            var propriedades = tipo.GetProperties();
+
+            var builder = new StringBuilder();
+            builder.AppendLine($"Data de log: {DateTime.Now}");
+            //Jeito Antigo -> builder.AppendLine($"Nome da pessoa: {pessoa.Nome}");
+            // usando Reflection:
+            foreach (var p in propriedades)
+            {
+                builder.AppendLine($"{p.Name}: {p.GetValue(obj)}");
+            }
+            builder.AppendLine("-------------");
+            var texto = builder.ToString();
+
+            Console.WriteLine(texto);
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public string Profissao { get; set; }
+    }
+
+    public class Equipamento
+    {
+        public string Tipo { get; set; }
+        public string Marca { get; set; }
+        public int Quantidade { get; set; }
+    }
+
+    public class Livro
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+
+        public int Edicao { get; set; }
+    }
+}
